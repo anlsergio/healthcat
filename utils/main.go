46d6044da52c63a8402383570f9b320e758e8abb
Anderson L. Sergio <anderson.als04@gmail.com>
2021-06-01 17:18:32 -0300

    Append the project dir abs. path to config file

--- utils/main.go
+++ utils/main.go
@@ -1,6 +1,9 @@
 package utils
 
 import (
+	"path"
+	"path/filepath"
+	"runtime"
 	"strings"
 
 	"github.com/spf13/viper"
@@ -20,4 +23,11 @@ func LoadConfig(filePath string, fileName string) error {
 	}
 
 	return nil
+}
+
+// RootDir returns the root directory of the project
+func RootDir() string {
+	_, b, _, _ := runtime.Caller(0)
+	d := path.Join(path.Dir(b))
+	return filepath.Dir(d)
 }
