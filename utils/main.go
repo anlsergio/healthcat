9995483d865f37925acaf0ac5e8dcf8235fec21e
Anderson L. Sergio <anderson.als04@gmail.com>
2021-06-01 15:42:48 -0300

    Parse env. variables when loading configuration

--- utils/main.go
+++ utils/main.go
@@ -6,14 +6,18 @@ import (
 	"github.com/spf13/viper"
 )
 
-// LoadConfig loads system parameters from a config file
+// LoadConfig loads system parameters from a config file and from enviroment variables if they are defined
 func LoadConfig(filePath string, fileName string) error {
 	viper.SetConfigName(strings.Split(fileName, ".")[0])
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath(filePath)
 
+	viper.AutomaticEnv()
+	viper.SetEnvPrefix("healthcat")
+
 	if err := viper.ReadInConfig(); err != nil {
 		return err
 	}
+
 	return nil
 }
