f8cba7e9682b839d62b487307b3e5bd4d1299cf1
Anderson L. Sergio <anderson.als04@gmail.com>
2021-06-08 10:30:03 -0300

    Allow for graceful config file parsing

--- cmd/config.go
+++ cmd/config.go
@@ -30,10 +30,10 @@ func LoadConfig(cmd *cobra.Command, filePath string, fileName string) error {
 	v.SetEnvPrefix(configEnvPrefix)
 
 	if err := v.ReadInConfig(); err != nil {
-		// if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
-		// 	return err
-		// }
-		return err
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return err
+		}
+		fmt.Fprintf(os.Stdout, "couldn't load system parameters from the provided file %s. Using default parameters respecting the precedence order instead.\n", fileName)
 	} else {
 		fmt.Fprintf(os.Stdout, "configuration file found. Loading system parameters from file %s\n", fileName)
 	}
@@ -45,17 +45,14 @@ func LoadConfig(cmd *cobra.Command, filePath string, fileName string) error {
 
 func bindFlags(cmd *cobra.Command, v *viper.Viper) {
 	cmd.Flags().VisitAll(func(f *pflag.Flag) {
-		// fmt.Printf("Flag: '%v', corresponding value from source: '%v'\n", f.Name, v.Get(f.Name))
 		if strings.Contains(f.Name, "-") {
 			envVarSuffix := strings.ToUpper(strings.ReplaceAll(f.Name, "-", "_"))
 			v.BindEnv(f.Name, fmt.Sprintf("%s_%s", configEnvPrefix, envVarSuffix))
-			// fmt.Printf("Changing variables: %s_%s\n", configEnvPrefix, envVarSuffix)
 		}
 
 		if !f.Changed && v.IsSet(f.Name) {
 			val := v.Get(f.Name)
 			cmd.Flags().Set(f.Name, fmt.Sprintf("%v", val))
-			// fmt.Println("Setting unset parameters from configuration source: ", val)
 		}
 	})
 	// v.WriteConfigAs("./config_generated_from_viper.yml")
